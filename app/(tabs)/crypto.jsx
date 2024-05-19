2495b562623e7840519e00f8b6a42b6f69d4ef18
esse.jacques.co <essedansomon@gmail.com>
2024-05-19 13:00:26 +0000

    setup config

--- app/(tabs)/crypto.jsx
+++ app/(tabs)/crypto.jsx
@@ -1,11 +1,61 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { useQuery } from '@tanstack/react-query';
+import { Link } from 'expo-router';
+// import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import type {Currency} from "../../interfaces/crypto";
+import {useHeaderHeight} from "@react-navigation/elements";
+import {Colors} from "../../constants/Colors";
 
-const Crypto = () => {
+const Page = () => {
+    const headerHeight = useHeaderHeight();
+
+    const currencies = useQuery({
+        queryKey: ['listings'],
+        queryFn: () => fetch('/api/listings').then((res) => res.json()),
+    });
+
+    const ids = currencies.data?.map((currency: Currency) => currency.id).join(',');
+
+    const { data } = useQuery({
+        queryKey: ['info', ids],
+        queryFn: () => fetch(`/api/info?ids=${ids}`).then((res) => res.json()),
+        enabled: !!ids,
+    });
+
     return (
-        <View>
-            <Text>Crypto tes</Text>
-        </View>
-    )
-}
-export default Crypto
+        <ScrollView
+            style={{ backgroundColor: Colors.background }}
+            contentContainerStyle={{ paddingTop: headerHeight }}>
+            <Text >Latest Crypot</Text>
+            <View >
+                {currencies.data?.map((currency: Currency) => (
+                    <Link href={`/crypto/${currency.id}`} key={currency.id} asChild>
+                        <TouchableOpacity style={{ flexDirection: 'row', gap: 14, alignItems: 'center' }}>
+                            <Image source={{ uri: data?.[currency.id].logo }} style={{ width: 40, height: 40 }} />
+                            <View style={{ flex: 1, gap: 6 }}>
+                                <Text style={{ fontWeight: '600', color: Colors.dark }}>{currency.name}</Text>
+                                <Text >{currency.symbol}</Text>
+                            </View>
+                            <View style={{ gap: 6, alignItems: 'flex-end' }}>
+                                <Text>{currency.quote.EUR.price.toFixed(2)} €</Text>
+                                <View style={{ flexDirection: 'row', gap: 4 }}>
+                                    <Ionicons
+                                        name={currency.quote.EUR.percent_change_1h > 0 ? 'caret-up' : 'caret-down'}
+                                        size={16}
+                                        color={currency.quote.EUR.percent_change_1h > 0 ? 'green' : 'red'}
+                                    />
+                                    <Text
+                                        style={{ color: currency.quote.EUR.percent_change_1h > 0 ? 'green' : 'red' }}>
+                                        {currency.quote.EUR.percent_change_1h.toFixed(2)} %
+                                    </Text>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                    </Link>
+                ))}
+            </View>
+        </ScrollView>
+    );
+};
+export default Page;
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -7,9 +7,16 @@ import 'react-native-reanimated';
 
 import {useColorScheme} from '@/hooks/useColorScheme';
 import {FontAwesome} from "@expo/vector-icons";
+import {StatusBar} from "expo-status-bar";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { UserInactivityProvider } from '@/context/UserInactivity';
+const queryClient = new QueryClient();
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
 
 
 export default function RootLayout() {
@@ -32,13 +39,25 @@ export default function RootLayout() {
     }
 
     return (
-        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-            <Stack>
-                <Stack.Screen name="index" options={{headerShown: false}}/>
-                <Stack.Screen name="(auth)" options={{headerShown: false}}/>
-                <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-                <Stack.Screen name="+not-found"/>
-            </Stack>
-        </ThemeProvider>
+        // <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache}>
+            <QueryClientProvider client={queryClient}>
+                {/*<UserInactivityProvider>*/}
+                    <GestureHandlerRootView style={{ flex: 1 }}>
+                        <StatusBar style="light" />
+                        {/*<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>*/}
+                            <Stack>
+                                <Stack.Screen name="index" options={{headerShown: false}}/>
+                                <Stack.Screen name="(auth)" options={{headerShown: false}}/>
+                                <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                                <Stack.Screen name="+not-found"/>
+                            </Stack>
+                        {/*</ThemeProvider>*/}
+                    </GestureHandlerRootView>
+                {/*</UserInactivityProvider>*/}
+            </QueryClientProvider>
+        // </ClerkProvider>
+
     );
 }
+
+
